9fbe4303054d44a8f223ea3fc73d9df6a8db18a4
jedariana <jedariana@gmail.com>
2021-06-14 20:21:00 -0400

    puerto

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ require('./routes/distribuidor.routes')(app);
 
 
 
-const port = process.env.port || 5000
+const port = process.env.PORT || 5000
 app.listen(port, () => {
     console.log(`Serve at http://localhost:${port}`);
 }).l;
